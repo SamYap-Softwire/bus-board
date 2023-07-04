8a78b9c45cd99bd1a62cdc8d50381c41485a5ab7
SamYap-Softwire <samyun.yap@softwire.com>
2023-07-04 14:56:13 +0100

    Styled navigation bar

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,15 +3,15 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <nav>
+      <nav className="navbar">
         <table>
           <tr>
-            <td><Link to="/">Home</Link></td>
-            <td><Link to="/helloworld">HelloWorld</Link></td>
+            <td className="links"><Link to="/">Home</Link></td>
+            <td className="links"><Link to="/helloworld">HelloWorld</Link></td>
           </tr>
         </table>
       </nav>
-      <Outlet />
+      <Outlet/>
     </>
   );
 };
